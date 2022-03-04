c0da3e71604737302fd73b5e83059ab2a4477874
LucasBmm <lucassbomfimm@gmail.com>
2022-03-04 16:02:24 -0300

    Covariância

--- script.js
+++ script.js
@@ -10,7 +10,18 @@ function desvioPadrao(list) {
   return Math.sqrt(variancia(list));
 }
 
-function covar() {}
+function covar(list1, list2) {
+  if (list1.length !== list2.length) {
+    return alert('As duas listas precisam ser do mesmo tamanho');
+  }
+  let sum = 0;
+  let media1 = calcMedia(list1);
+  let media2 = calcMedia(list2);
+  for (let i = 0; i < list1.length; i++)
+    sum = sum + (list1[i] - media1) * (list2[i] - media2);
+
+  return sum / (list1.length - 1);
+}
 
 function rmse() {}
 
