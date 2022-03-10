89547f344a530146842592a93307749911a3b0db
VictorAcarvalho <carvalho.victorfa@gmail.com>
2022-03-09 22:29:57 -0300

    feat: refatora a modelagem da resposta da api e populate do select

--- script.js
+++ script.js
@@ -1,67 +1,66 @@
 const graphics = document.getElementById("graphs");
-const baseURL ='https://apitempo.inmet.gov.br/estacoes/T';
+const baseURL = "https://apitempo.inmet.gov.br/estacoes";
 
+async function getApi() {
+  const response = (await fetch(`${baseURL}/T`)).json();
+  return response;
+}
 
-async function getApi(){
-  const response =  (await fetch(baseURL)).json();
-  return response; 
+async function getWindInfoByDate(initDate, finalDate, id) {
+  const response = (
+    await fetch(`${baseURL}/${initDate}/${finalDate}/${id}`)
+  ).json();
+  return response;
+}
+
+async function getStations() {
+  const data = await getApi();
+  const stations = data.map((station) =>
+    Object({
+      id: station.CD_ESTACAO,
+      value: station.DC_NOME,
+    })
+  );
+
+  return stations;
+}
+
+const populatedSelect = async () => {
+  const stationSelect = document.getElementById("chose-station");
+  const stations = await getStations();
+  return stations.map((station) => {
+    const options = document.createElement("option");
+    options.setAttribute("value", station["id"]);
+    options.innerHTML = station["value"];
+    stationSelect.appendChild(options);
+  });
+};
+window.addEventListener(onload, populatedSelect());
+
+const processRequest = async () => {
+    // TODO: pegar info dos valores do input
+    const windeData = await getWindInfoByDate()
 };
 
+const trace1 = {
+  x: [1, 2, 3, 4],
 
-async function getIdList(){
-  const data = await getApi();
-  let stationsIDList = data.map(obj=>
-    obj.CD_ESTACAO
-    );
-    return stationsIDList;
-  };
-  
-  const populatedSelect =async()=>{
-    const stationSelect = document.getElementById("chose-station"); 
-    const data = await getIdList(); 
-    return data.map(id=>{
-      const options= document.createElement("option");
-      options.setAttribute("value",id);
-      options.innerHTML=id;
-      stationSelect.appendChild(options);
-  })};
-    window.addEventListener(onload, populatedSelect());
-  
+  y: [10, 15, 13, 17],
 
+  type: "scatter",
+};
 
+const trace2 = {
+  x: [1, 2, 3, 4],
 
+  y: [16, 5, 11, 9],
 
+  type: "scatter",
+};
 
+const data = [trace1, trace2];
+const layout = {
+  title: "Wind Dashboard",
+};
 
-
-
-
-
-var trace1 = {
-
-    x: [1, 2, 3, 4],
-  
-    y: [10, 15, 13, 17],
-  
-    type: 'scatter'
-  
-  };
-  
-  
-  var trace2 = {
-  
-    x: [1, 2, 3, 4],
-  
-    y: [16, 5, 11, 9],
-  
-    type: 'scatter'
-  
-  };
-  
-  
-  var data = [trace1, trace2];
-  var layout= {
-    title:'Wind Dashboard'
-  }
-  
-  Plotly.newPlot(graphics, data,layout , {scrollZoom:true});
+Plotly.newPlot(graphics, data, layout, { scrollZoom: true });
