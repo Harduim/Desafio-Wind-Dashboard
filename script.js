5f103069640fa97a84d1f1b4880ac9293a4ea67a
VictorAcarvalho <carvalho.victorfa@gmail.com>
2022-03-10 10:57:05 -0300

    feat: implementa o render do grafico

--- script.js
+++ script.js
@@ -1,82 +1,6 @@
-const graphics = document.getElementById("graphs");
 const baseURL ='https://apitempo.inmet.gov.br/'
 
 //calcula a média de uma lista
-const average = (array) =>{
-    const averageOfArray = array.reduce((a,b)=> a+b, 0 );
-    const result = averageOfArray / array.length;
-    return result;
-};
-
-//calcula a variancia
-const variance = (array)=>{
-  const resolvedAverage= average(array);
-  const squareDiffs = array.map((value)=>{
-    const diff = value - resolvedAverage;
-    return diff * diff;
-  });
-  const variance = resolvedAverage(squareDiffs);
-  return variance  
-};
-
-// calcula o desvio padrão
-const standDeviation =(variance) =>{
-   return Math.sqrt(variance);
-};
-
-//calcula a covariancia
-const covariance = (array1, array2) =>{
-  let sum = 0;
-  const arrayLength = array1.length || array2.length;
-  const averageFirstArr = average(array1);
-  const averageSecondArr = average(array2)    
-  for(let i =0;i<arrayLength; i++){
-     sum = sum + (array1[i] - averageFirstArr) * (array2[i] -  averageSecondArr);
-  }
-  return sum / arrayLength;
-};
-
-//calcula o rmse
-const rmse = (arr) =>{
- return  Math.sqrt(arr.
-    map(val=> val*val).reduce((total,value)=>total+value)/arr.length
- );};
-
-//calcula a progressão linear
-const linearProgression = (xArray , yArray )=>{
-  const xSum=0, ySum=0 , xxSum=0, xySum=0;
-  const count = xArray.length;
-  for (let i = 0; i < count; i++) {
-  xSum += xArray[i];
-  ySum += yArray[i];
-  xxSum += xArray[i] * xArray[i];
-  xySum += xArray[i] * yArray[i];
-};
-  const slope = (count * xySum - xSum * ySum) / (count * xxSum - xSum * xSum);
-  const intercept = (ySum / count) - (slope * xSum) / count;
-  const xValues = []; 
-  const yValues = [];
-  for (let x = xArray[0]; x <= xArray.length; x += 1) {
-    xValues.push(x);
-    yValues.push(x * slope + intercept);
-  }     
-    const linearResult={
-      slope: xValues,
-      intercept: yValues
-    }
-    return linearResult
-};
-
-//calcula a predict
-const predict =(arr1 , arr2 , val) =>{
-   const first =  arr1[0];
-   const second = arr2[0];
-   const firstValue = arr1[1];
-   const secondValue = arr2[1];
-   const a = (firstValue - secondValue)/(first-second)
-   const b = second - (second*a);
-   return val *b;
-}
 
 
 //conecta a api
@@ -107,10 +31,11 @@ async function getStations(){
       stationSelect.appendChild(options);
     });
   };
-
   
-
+  
+  
   const getWindBydDate = async () =>{
+    const graphics = document.getElementById("graphs");
     //pega todos os valores dos inputs
     const stationId= document.getElementById("chose-station").value
     const initialDate = document.getElementById("initialDate").value
@@ -163,10 +88,158 @@ async function getStations(){
           velTimeTable,
           nameStation
         };
-    
 
-      
-  };
+
+        const average = (array) =>{
+          const averageOfArray = array.reduce((a,b)=> a+b, 0 );
+            const result = averageOfArray / array.length;
+            return result;
+        };
+        
+        //calcula a variancia
+        const variance = (array)=>{
+          const resolvedAverage= average(array);
+          const squareDiffs = array.map((value)=>{
+            const diff = value - resolvedAverage;
+            return diff * diff;
+          });
+          const variance = resolvedAverage(squareDiffs);
+          return variance  
+        };
+        
+        // calcula o desvio padrão
+        const standDeviation =(variance) =>{
+          return Math.sqrt(variance);
+        };
+        
+        //calcula a covariancia
+        const covariance = (array1, array2) =>{
+          let sum = 0;
+          const arrayLength = array1.length || array2.length;
+          const averageFirstArr = average(array1);
+          const averageSecondArr = average(array2)    
+          for(let i =0;i<arrayLength; i++){
+             sum = sum + (array1[i] - averageFirstArr) * (array2[i] -  averageSecondArr);
+          }
+          return sum / arrayLength;
+        };
+        
+        //calcula o rmse
+        const rmse = (arr) =>{
+         return  Math.sqrt(arr.
+            map(val=> val*val).reduce((total,value)=>total+value)/arr.length
+         );};
+        
+        //calcula a progressão linear
+        const linearRegression = (xArray , yArray )=>{
+              const agregor = {};
+              const xAverage = average(xArray);
+              const yAverage = average(yArray);
+        
+              let slope =0;
+              let slopeCount =0;
+              let slopeBase = 0;
+              for (i =0; i<xArray.length; i++){
+                slopeCount += (xArray[i]-xAverage)*(yArray[i]-yAverage);
+                slopeBase += Math.pow((xArray[i]-xAverage),2)
+              }
+              slope= slopeCount/slopeBase;
+              agregor['slope'] = slope; 
+              
+              const intercept = yAverage - xAverage*slope;
+              agregor['intercept'] = intercept;
+              console.log(agregor);
+              return agregor;
+          };
+        
+          //calcula a predict
+          const predict =(arr1 , arr2 ) =>{
+           const first =  arr1[0];
+           const second = arr2[0];
+           const firstValue = arr1[1];
+           const secondValue = arr2[1];
+           const a = (firstValue - secondValue)/(first-second)
+           const b = second - (second*a);
+           return  a*b;
+        }
+        
+
+
+
+
+        
+        if(serieType === '1'){
+          const lr =  await  linearRegression(serieHoraria.velTimeTable,serieHoraria.velTimeTable);
+         
+          const previst =  predict(lr,serieHoraria.velTimeTable);
+
+
+         var trace1 = {
+           
+           x: serieHoraria.velTimeTable.length,
+           
+           y: serieHoraria.velTimeTable,
+           
+           type: 'scatter'
+           
+          };
+          
+          
+          var trace2 = {
+            
+            x: serieHoraria.velTimeTable.length,
+            
+            y: previst,
+            
+            type: 'scatter'
+            
+          };
+          
+          
+          var data = [trace1, trace2];
+          var layout= {
+            title:`${serieHoraria.nameStation}`
+          }
+          
+          Plotly.newPlot(graphics, data,layout , {scrollZoom:true});
+          
+        } 
+         else{
+          
+
+              var trace1 = {
+              
+                x: serieDiaria.windAverage.length,
+                
+                y: serieDiaria.windAverage,
+                
+                type: 'scatter'
+                
+              };
+              
+              
+              var trace2 = {
+                
+                x: serieDiaria.windAverage,
+                
+                y: serieDiaria.windAverage.length,
+                
+                type: 'scatter'
+                
+              };
+              
+              
+              var data = [trace1, trace2];
+              var layout= {
+                title:`${serieDiaria.nameStation}`
+              }
+              
+              Plotly.newPlot(graphics, data,layout , {scrollZoom:true});
+              
+         }
+          
+          
+        };
   
   
   
@@ -183,31 +256,3 @@ async function getStations(){
 
 
 
-var trace1 = {
-
-    x: [1, 2, 3, 4],
-  
-    y: [10, 15, 13, 17],
-  
-    type: 'scatter'
-  
-  };
-  
-  
-  var trace2 = {
-  
-    x: [1, 2, 3, 4],
-  
-    y: [16, 5, 11, 9],
-  
-    type: 'scatter'
-  
-  };
-  
-  
-  var data = [trace1, trace2];
-  var layout= {
-    title:'Wind Dashboard'
-  }
-  
-  Plotly.newPlot(graphics, data,layout , {scrollZoom:true});
